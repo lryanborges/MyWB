b93c1c01add67f3b8aceaf6b683eb5ba2bb63a6b
lryanborges <ryanborgeslockser@gmail.com>
2024-08-30 11:24:58 -0300

    alt: assets e navbar

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,3 +3,8 @@ import 'package:flutter/material.dart';
 const Color tdBG = Color(0xFF0E1217);
 const Color tdRed = Color(0xFF8B0002);
 const Color tdContainer = Color(0xFF151F2D);
+const Color tdAccentColor = Color(0xFFE0FBFC);
+const Color tdDarkerColor = Color(0xFF080A0D);
+const Color tdFontColor = Color(0xFFFCF7F8);
+const Color tdWhite = Color(0xFFFCF7F8);
+const Color tdSelected = Color(0xFF293241);
--- lib/pages/init/init.dart
+++ lib/pages/init/init.dart
@@ -1,56 +1,49 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_wb/constants/colors.dart';
+import 'package:my_wb/widgets/my_nav_bar.dart';
 
 class Init extends StatefulWidget {
   final String name;
 
-  const Init({Key? key, required this.name}): super(key: key);
+  const Init({Key? key, required this.name}) : super(key: key);
 
   @override
   State<Init> createState() => _InitialPageState();
-
 }
 
 class _InitialPageState extends State<Init> {
   int _currentIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+    print('Selected Index: $index');
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      body: const Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-                "Menu",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal
-                )
-            )
-          ],
-        )
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.blue,
-        iconSize: 30,
-        selectedFontSize: 20,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile", backgroundColor: Colors.red),
-          BottomNavigationBarItem(icon: Icon(Icons.ac_unit_outlined), label: "Test2", backgroundColor: Colors.yellow),
+      backgroundColor: tdBG,
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child:  Text(
+              "Menu",
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
         ],
+      ),
+      bottomNavigationBar: MyNavBar(
+        currentIndex: _currentIndex, // Atualiza o índice atual
+        onTap: _onItemTapped, // Passa a função de callback
       ),
     );
   }
-
 }
